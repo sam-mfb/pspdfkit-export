26309f90d5dccddbeb49fac56e15a00e66c5f671
sam <sam@mfbtech.com>
2021-10-19 12:43:11 -0400

    add instantJsons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import PSPDFKit from "pspdfkit"
 import { useRef } from "react"
 import "./App.css"
+import { instantJsonNoId } from "./instantJsonNoId"
+import { instantJsonWithId } from "./instantJsonWithId"
 
 const PSPDFKIT_REQUIRED_HEIGHT = "100%"
 const PSPDFKIT_REQUIRED_WIDTH = "100%"
 const BASE_URL = `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`
 
 function App() {
-  const loadPdf = () => {
+  const loadPdf = (instantJSON?: any) => {
     if (pdfContainer.current === null) return
     if (pdfContainer.current) {
       PSPDFKit.unload(pdfContainer.current)
@@ -15,14 +17,22 @@ function App() {
     PSPDFKit.load({
       document: BASE_URL + "/example.pdf",
       container: pdfContainer.current,
-      baseUrl: BASE_URL
-    })
+      baseUrl: BASE_URL,
+      instantJSON
+    }).then(instance =>
+      instance.exportInstantJSON().then(ij => console.log(ij))
+    )
   }
   const pdfContainer = useRef<HTMLDivElement>(null)
   return (
     <div className="App">
       <div>
-        <button onClick={loadPdf}>Load PDF</button>
+        <button onClick={() => loadPdf(instantJsonWithId)}>
+          Load InstantJSON With Attribute ID
+        </button>
+        <button onClick={() => loadPdf(instantJsonNoId)}>
+          Load InstantJSON Without Attribute ID
+        </button>
       </div>
       <div
         className="pdf-viewer"
---
+++ src/instantJsonNoId.ts
@@ -0,0 +1,44 @@
+export const instantJsonNoId = {
+  format: "https://pspdfkit.com/instant-json/v1",
+  pdfId: {
+    changing: "bkwtMFYaQRClFPnanzwEaw==",
+    permanent: "dODcGjkiRfKMhIAxP9gLVQ=="
+  },
+  annotations: [
+    {
+      name: "01FA0YD5WVSAT8WVFVN9TKE39E",
+      v: 1,
+      pageIndex: 0,
+      bbox: [
+        252.2028916810364, 16.26397705078125, 57.29099984854602,
+        11.256020294830371
+      ],
+      opacity: 1,
+      createdAt: "2021-07-07T16:56:50.843Z",
+      updatedAt: "2021-07-07T16:56:50.843Z",
+      creatorName: "Omar Little",
+      customData: null,
+      isCommentThreadRoot: false,
+      type: "pspdfkit/markup/highlight",
+      rects: [
+        [
+          252.2028916810364, 16.26397705078125, 57.29099984854604,
+          11.256020294830371
+        ]
+      ],
+      color: "#fcee7c",
+      blendMode: "multiply"
+    }
+  ],
+  bookmarks: [
+    {
+      id: "01FA0YDHAJCV27DMFJM5HP027S",
+      v: 1,
+      pdfBookmarkId: null,
+      type: "pspdfkit/bookmark",
+      name: "My Bookmark",
+      sortKey: null,
+      action: { type: "goTo", pageIndex: 0 }
+    }
+  ]
+}
