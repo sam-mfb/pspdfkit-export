d14961fa2aa2cc63be05e23e220a2480ef155f67
Sam Davidoff <sam@mfbtech.com>
2021-10-20 14:23:40 -0400

    fix loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,42 @@
-import PSPDFKit from "pspdfkit"
-import { useRef } from "react"
-import "./App.css"
-import { instantJsonNoId } from "./instantJsonNoId"
-import { instantJsonWithId } from "./instantJsonWithId"
+import PSPDFKit from "pspdfkit";
+import { useRef } from "react";
+import "./App.css";
 
-const PSPDFKIT_REQUIRED_HEIGHT = "100%"
-const PSPDFKIT_REQUIRED_WIDTH = "100%"
-const BASE_URL = `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`
+const PSPDFKIT_REQUIRED_HEIGHT = "100%";
+const PSPDFKIT_REQUIRED_WIDTH = "100%";
+const BASE_URL = `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`;
 
 function App() {
   const loadPdf = (instantJSON?: any) => {
-    if (pdfContainer.current === null) return
+    if (pdfContainer.current === null) return;
     if (pdfContainer.current) {
-      PSPDFKit.unload(pdfContainer.current)
+      PSPDFKit.unload(pdfContainer.current);
     }
     PSPDFKit.load({
-      document: BASE_URL + "/example.pdf",
+      document: BASE_URL + "noIDPdf.pdf",
       container: pdfContainer.current,
       baseUrl: BASE_URL,
-      instantJSON
-    }).then(instance =>
-      instance.exportInstantJSON().then(ij => console.log(ij))
-    )
-  }
-  const pdfContainer = useRef<HTMLDivElement>(null)
+      instantJSON,
+    }).then((instance) =>
+      instance.exportInstantJSON().then((ij) => console.log(ij))
+    );
+  };
+  const pdfContainer = useRef<HTMLDivElement>(null);
   return (
     <div className="App">
       <div>
-        <button onClick={() => loadPdf(instantJsonWithId)}>
-          Load InstantJSON With Attribute ID
-        </button>
-        <button onClick={() => loadPdf(instantJsonNoId)}>
-          Load InstantJSON Without Attribute ID
-        </button>
+        <button onClick={() => loadPdf()}>Load InstantJSON</button>
       </div>
       <div
         className="pdf-viewer"
         ref={pdfContainer}
         style={{
           width: PSPDFKIT_REQUIRED_WIDTH,
-          height: PSPDFKIT_REQUIRED_HEIGHT
+          height: PSPDFKIT_REQUIRED_HEIGHT,
         }}
       ></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
